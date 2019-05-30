91b2a390777ba7219cf37028d88ce00c5d5cbf97
Ava <ava@example.com>
2019-05-30 18:14:16 -0400

    adding slider files

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,8 +1,11 @@
 {
-   $('button#play-pause').on('click', function() {
-     player.playPause();
+  $('button#play-pause').on('click', function() {
+     helper.playPauseAndUpdate();
      $(this).attr('playState', player.playState);
    });
+
+
+
 
    $('button#next').on('click', function() {
      if (player.playState !== 'playing') { return; }
@@ -12,29 +15,40 @@
      if (nextSongIndex >= album.songs.length) { return; }
 
      const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     helper.playPauseAndUpdate(nextSong);
    });
+
+
 
    $('button#previous').on('click', function() {
      if (player.playState !== 'playing') { return; }
 
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex - 1;
-     if (nextSongIndex >= album.songs.length) { return; }
+     const previousSongIndex = currentSongIndex - 1;
+     if (previousSongIndex >= album.songs.length) { return; }
 
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
+     const previousSong = album.songs[previousSongIndex];
+     helper.playPauseAndUpdate(previousSong);
    });
 
-      $('#time-control input').on('input', function (event) {
-      player.skipTo(event.target.value);
-      });
 
-      setInterval( () => {
-      if (player.playState !== 'playing') { return; }
-      const currentTime = player.getTime();
+
+   $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
+     });
+
+   setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text( currentTime );
      $('#time-control input').val(percent);
-      }, 1000);
- }
+  }, 1000);
+}
+
+
+
+$('#volume-control input').on('input', function (evcent){
+  player.setVolume(event.target.value);
+});
